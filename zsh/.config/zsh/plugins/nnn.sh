76c25cbe8e98dd84b8c5b8c4208db2221592e915
Shreeram Modi <67710369+Shrimpram@users.noreply.github.com>
2021-05-10 12:22:28 -0700

    Add pdfview plugin

--- zsh/.config/zsh/plugins/nnn.sh
+++ zsh/.config/zsh/plugins/nnn.sh
@@ -2,7 +2,7 @@ export NNN_FIFO=/tmp/nnn.fifo
 
 export NNN_USE_EDITOR=1
 
-export NNN_PLUG='f:fzopen;v:imgview;p:preview-tui'
+export NNN_PLUG='f:fzopen;v:imgview;p:preview-tui;d:pdfview;'
 
 n ()
 {
