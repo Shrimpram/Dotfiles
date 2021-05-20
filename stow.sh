e6c60b48a348c6d3d2e4499bb1597a4910eb0545
Shreeram Modi <67710369+Shrimpram@users.noreply.github.com>
2021-05-20 16:53:46 -0700

    Update stow script to allow more options

--- stow.sh
+++ stow.sh
@@ -1,9 +1,21 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 read -p "What do you want to do? ([S]tow/[D]elete links/[R]estow): " x
 
-echo "Stowing binaries"
-
 cd binaries
+
+if [ "$x" = S ]
+then
+    echo "Stowing binaries"
+elif [ "$x" = D ]
+then
+    echo "Deleting symlinks"
+elif [ "$x" = R ]
+then
+    echo "Restowing binaries"
+else
+    echo "Invalid input, aborting"
+    exit
+fi
 
 sudo stow -v -"$x" * -t /usr/local/
