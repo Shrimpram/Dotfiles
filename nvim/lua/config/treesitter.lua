cfe49481a2903cdb69fa28249804093767e23c85
Shreeram Modi <67710369+Shrimpram@users.noreply.github.com>
2021-06-04 21:48:15 -0700

    Add java to treesitter languages

--- nvim/lua/config/treesitter.lua
+++ nvim/lua/config/treesitter.lua
@@ -2,7 +2,7 @@ local ts_configs = require('nvim-treesitter.configs')
 ts_configs.setup {
   ensure_installed = {
     'bash', 'bibtex', 'c', 'c_sharp', 'comment', 'cpp', 'css', 'go',
-    'html', 'javascript', 'json', 'jsonc', 'latex', 'lua',
+    'html', 'java', 'javascript', 'json', 'jsonc', 'latex', 'lua',
     'python', 'regex', 'rust', 'toml', 'yaml'
   },
   highlight = {enable = true, use_languagetree = true},
