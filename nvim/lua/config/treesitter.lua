5d508d4f63d2ce87ece49490c12c2c4c1f96d0c8
Shreeram Modi <67710369+Shrimpram@users.noreply.github.com>
2021-06-06 01:48:20 -0700

    Reformat treesitter config

--- nvim/lua/config/treesitter.lua
+++ nvim/lua/config/treesitter.lua
@@ -1,10 +1,12 @@
 local ts_configs = require('nvim-treesitter.configs')
 ts_configs.setup {
-  ensure_installed = {
-    'bash', 'bibtex', 'c', 'c_sharp', 'comment', 'cpp', 'css', 'go',
-    'html', 'java', 'javascript', 'json', 'jsonc', 'latex', 'lua',
-    'python', 'regex', 'rust', 'toml', 'yaml'
-  },
-  highlight = {enable = true, use_languagetree = true},
-  indent = {enable = false},
+    ensure_installed = {
+        'bash', 'bibtex', 'c', 'c_sharp', 'comment', 'cpp', 'css', 'go',
+        'html', 'java', 'javascript', 'json', 'jsonc', 'latex', 'lua',
+        'python', 'regex', 'rust', 'toml', 'yaml'
+    },
+    highlight = {
+        enable = true,
+        use_languagetree = true,
+    }
 }
