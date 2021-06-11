6ef440f34dc14a95e4c8e377696ed1435ac6076c
Shreeram Modi <67710369+Shrimpram@users.noreply.github.com>
2021-06-10 22:28:17 -0700

    Remove yabai from home
    Using https://rectangleapp.com/ now

--- nix/home.nix
+++ nix/home.nix
@@ -23,49 +23,4 @@
   # $ darwin-rebuild changelog
   system.stateVersion = 4;
 
-  services.yabai = {
-    enable = true;
-    package = pkgs.yabai;
-    enableScriptingAddition = false;
-    config = {
-        window_placement             = "first_child";
-
-        window_border                = "off";
-        window_border_placement      = "inset";
-        window_border_width          = 2;
-        window_border_radius         = 3;
-        active_window_border_topmost = "off";
-        window_topmost               = "on";
-
-        window_shadow                = "off";
-        active_window_border_color   = "0xff5c7e81";
-        normal_window_border_color   = "0xff505050";
-        insert_window_border_color   = "0xffd75f5f";
-        active_window_opacity        = "1.0";
-        normal_window_opacity        = "1.0";
-
-        split_ratio                  = "0.50";
-        auto_balance                 = "on";
-
-        mouse_modifier               = "fn";
-        mouse_action1                = "move";
-        mouse_action2                = "resize";
-
-        layout                       = "bsp";
-
-        top_padding                  = 10;
-        bottom_padding               = 10;
-        left_padding                 = 10;
-        right_padding                = 10;
-        window_gap                   = 10;
-    };
-
-    extraConfig = ''
-        # rules
-        yabai -m rule --add app='System Preferences' manage=off
-        yabai -m rule --add app='Spotify' manage=off
-
-        # Any other arbitrary config here
-      '';
-    };
 }
